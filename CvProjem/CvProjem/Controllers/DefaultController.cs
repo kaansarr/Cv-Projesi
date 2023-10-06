bccb43ccb6652595ccf76529210a45fbf39cf64f
kaansarr <kaansar33@gmail.com>
2023-10-06 15:07:03 +0300

    up

---
+++ CvProjem/CvProjem/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CvProjem.Models.Entity;
+
+namespace CvProjem.Controllers
+{
+    public class DefaultController : Controller
+    {
+        
+        DbCvEntities db=new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler=db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler =db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+    }
+}
