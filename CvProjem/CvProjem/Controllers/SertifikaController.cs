e8b954d0866affb47b5cece9c2cb6671b9f38f13
kaansarr <kaansar33@gmail.com>
2023-10-11 17:45:28 +0300

    İletişim Paneli

--- CvProjem/CvProjem/Controllers/SertifikaController.cs
+++ CvProjem/CvProjem/Controllers/SertifikaController.cs
@@ -3,15 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CvProjem.Models.Entity;
+using CvProjem.Repositories;
 
 namespace CvProjem.Controllers
 {
     public class SertifikaController : Controller
     {
-        // GET: Sertifika
+        GenericRepository<TblSertifikalarım> repo=new GenericRepository<TblSertifikalarım>();
         public ActionResult Index()
+        {
+
+            var sertifika = repo.List();
+            return View(sertifika);
+        }
+        [HttpGet]
+        public ActionResult SertifikaGetir(int id)
+        {
+            var sertifika=repo.Find(x=>x.ID==id);
+            ViewBag.d = id;
+            return View(sertifika);
+        }
+
+        [HttpPost]
+        public ActionResult SertifikaGetir(TblSertifikalarım t)
+        {
+			var sertifika = repo.Find(x => x.ID ==t.ID);
+            sertifika.Aciklama=t.Aciklama;
+            sertifika.Tarih = t.Tarih;
+            repo.TUpdate(sertifika);
+			return RedirectToAction ("Index");
+        }
+        [HttpGet]
+        public ActionResult YeniSertifika()
         {
             return View();
         }
-    }
+        [HttpPost]
+		public ActionResult YeniSertifika(TblSertifikalarım p)
+
+		{
+            repo.TAdd(p);
+			return RedirectToAction("Index");
+		}
+
+        public ActionResult SertifikaSil(int id)
+        {
+            var sertifika=repo.Find(x=>x.ID==id);
+            repo.TDelete(sertifika);           
+            return RedirectToAction("Index");
+        }
+	}
 }
---
+++ CvProjem/CvProjem/Controllers/iletisimController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CvProjem.Models.Entity;
+using CvProjem.Repositories;
+
+namespace CvProjem.Controllers
+{
+    public class iletisimController : Controller
+    {
+          GenericRepository<Tbliletişim> repo = new GenericRepository<Tbliletişim>();
+        public ActionResult Index()
+        {
+            var mesajlar=repo.List();
+            return View(mesajlar);
+        }
+    }
+}
