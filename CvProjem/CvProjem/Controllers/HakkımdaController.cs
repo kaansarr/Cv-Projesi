eccbd95790ba8598f554e5135fe3db83d70efcc3
kaansarr <kaansar33@gmail.com>
2023-10-12 00:48:24 +0300

    up

--- CvProjem/CvProjem/Controllers/HakkımdaController.cs
+++ CvProjem/CvProjem/Controllers/HakkımdaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CvProjem.Models.Entity;
+using CvProjem.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,27 @@ namespace CvProjem.Controllers
 {
     public class HakkımdaController : Controller
     {
-        // GET: Hakkımda
+        DbCvEntities db = new DbCvEntities();
+        GenericRepository<TblHakkimda> repo = new GenericRepository<TblHakkimda>();
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var hakkimda = repo.List();
+            return View(hakkimda);
+        }
+        [HttpPost]
+        public ActionResult Index(TblHakkimda p)
+        {
+            var t = repo.Find(x => x.ID == 1);
+            t.Ad = p.Ad;
+            t.Soyad = p.Soyad;
+            t.Adres = p.Adres;
+            t.Mail = p.Mail;
+            t.Telefon = p.Telefon;
+            t.Aciklama = p.Aciklama;
+            t.Resim = p.Resim;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
         }
     }
 }
